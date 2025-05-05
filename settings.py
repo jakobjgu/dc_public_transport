febace0dcc49670bffc08204b5d926c7301fd817
Jakob <61227884+jakobjgu@users.noreply.github.com>
2025-05-05 17:01:17 +0300

    adding webhook listening and flask app to the repo

--- settings.py
+++ settings.py
@@ -4,6 +4,13 @@ from dotenv import load_dotenv, find_dotenv
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
+# fixed variables
+S3_BUCKET=os.getenv("S3_BUCKET")
+S3_STATION_LINE_MAP_PATH=os.getenv("S3_STATION_LINE_MAP_PATH")
+WMATA_DEMO_API_KEY=os.getenv("WMATA_DEMO_API_KEY")
+GITHUB_WEBHOOK_SECRET=os.getenv("GITHUB_WEBHOOK_SECRET")
+
+# paths
 if os.getenv('ENV') == 'EC2':
     base_path = os.getenv('EC2_BASE_PATH')
 else:
@@ -12,15 +19,9 @@ else:
 # Expand the ~ to the full home directory path
 base_path = os.path.expanduser(base_path)
 
-S3_BUCKET=os.getenv("S3_BUCKET")
-S3_STATION_LINE_MAP_PATH=os.getenv("S3_STATION_LINE_MAP_PATH")
-WMATA_DEMO_API_KEY=os.getenv("WMATA_DEMO_API_KEY")
-
-# paths
 PROJECT_SOURCE_FILES_BASE_DIR=os.getenv("PROJECT_SOURCE_FILES_BASE_DIR")
 PROJECT_TRANSFORMED_FILES_BASE_DIR=os.getenv("PROJECT_TRANSFORMED_FILES_BASE_DIR")
 PROJECT_DBT_BASE_DIR=os.getenv("PROJECT_DBT_BASE_DIR")
-
 PROJECT_SOURCE_FILESDIR=base_path + PROJECT_SOURCE_FILES_BASE_DIR
 PROJECT_TRANSFORMED_FILES_DIR=base_path + PROJECT_TRANSFORMED_FILES_BASE_DIR
 PROJECT_DBT_DIR=base_path + PROJECT_DBT_BASE_DIR
